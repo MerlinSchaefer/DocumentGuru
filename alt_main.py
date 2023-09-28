0628d677b45aba69f097d7a20778fc270ce1efbb
MerlinSchaefer <ms101196@googlemail.com>
2023-09-28 16:51:02 +0200

    Refactor into classes

--- alt_main.py
+++ alt_main.py
@@ -3,7 +3,7 @@ import timeit
 import argparse
 import yaml
 import os
-from box import Box
+from pydantic import BaseModel
 from langchain.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader, UnstructuredMarkdownLoader
@@ -11,16 +11,47 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain.llms import CTransformers
+from langchain.schema import Document
 from typing import List
 from src.prompts import base_qa_template
 
+class Config(BaseModel):
+    RETURN_SOURCE_DOCUMENTS: bool
+    VECTOR_COUNT: int
+    CHUNK_SIZE: int
+    CHUNK_OVERLAP: int
+    DATA_PATH: str
+    DB_FAISS_PATH: str
+    MODEL_TYPE: str
+    MODEL_BIN_PATH: str
+    MAX_NEW_TOKENS: int
+    TEMPERATURE: float
+    # Add other configuration parameters as needed
+
+    @classmethod
+    def load_from_file(cls, config_path: str) -> 'Config':
+        """
+        Load configuration parameters from a YAML file.
+
+        Args:
+            config_path (str): Path to the YAML configuration file.
+
+        Returns:
+            Config: An instance of the Config class with loaded configuration parameters.
+        """
+        with open(config_path, 'r', encoding='utf8') as file:
+            config_data = yaml.safe_load(file)
+            return cls(**config_data)
+
 
 class VectorStoreBuilder:
-    def __init__(self, config: Box, document_type: str):
+    def __init__(self, config: Config, document_type: str):
         self.config = config
         self.document_type = document_type
         self.logger = logging.getLogger(__name__)
 
+
+    
     def build_vectorstore(self):
         try:
             # Set loader_cls and glob based on document_type
@@ -47,7 +78,7 @@ class VectorStoreBuilder:
                 chunk_size=self.config.CHUNK_SIZE,
                 chunk_overlap=self.config.CHUNK_OVERLAP
             )
-            texts: Texts = text_splitter.split_documents(documents)
+            texts: List[Document] = text_splitter.split_documents(documents)
 
             # Create embeddings
             embeddings = HuggingFaceEmbeddings(
@@ -65,71 +96,21 @@ class VectorStoreBuilder:
 
 
 class DocumentProcessor:
-    def __init__(self, config_path: str, input_text: str):
-        self.config_path = config_path
+    def __init__(self, config: Config, input_text: str):
+        self.config= config
         self.input_text = input_text
         self.logger = logging.getLogger(__name__)
         logging.basicConfig(level=logging.INFO)
 
-    def load_config(self) -> Box:
-        """
-        Load configuration parameters from a YAML file.
 
-        Returns:
-            Box: A Box object containing the configuration parameters.
-        """
-        with open(self.config_path, 'r', encoding='utf8') as file:
-            config = Box(yaml.safe_load(file))
-        return config
 
-    def build_vectorstore(self, config: Box) -> None:
-        """
-        Build a VectorStore from PDF documents.
 
-        Args:
-            config (Box): Configuration parameters including DATA_PATH, CHUNK_SIZE, CHUNK_OVERLAP, and DB_FAISS_PATH.
-
-        Returns:
-            None
-        """
-
-        try:
-            # Load the files from the data path
-            loader = DirectoryLoader(
-                path=config.DATA_PATH,
-                glob="*.pdf",
-                loader_cls=PyPDFLoader
-            )
-
-            documents = loader.load()
-
-            # Split documents into chunks
-            text_splitter = RecursiveCharacterTextSplitter(
-                chunk_size=config.CHUNK_SIZE,
-                chunk_overlap=config.CHUNK_OVERLAP
-            )
-            texts: Texts = text_splitter.split_documents(documents)
-
-            # Create embeddings
-            embeddings = HuggingFaceEmbeddings(
-                model_name='sentence-transformers/all-MiniLM-L6-v2',
-                model_kwargs={'device': 'cpu'}
-            )
-
-            vectorstore = FAISS.from_documents(texts, embeddings)
-            vectorstore.save_local(config.DB_FAISS_PATH)
-
-            self.logger.info("VectorStore created and saved successfully.")
-
-        except Exception as e:
-            self.logger.error(f"An error occurred while building VectorStore: {str(e)}")
-
-    def setup_dbqa(self, config: Box) -> RetrievalQA:
+    def setup_dbqa(self, config: Config) -> RetrievalQA:
         """
         Setup and configure a RetrievalQA object.
 
         Args:
-            config (Box): Configuration parameters including MODEL_BIN_PATH, MODEL_TYPE, MAX_NEW_TOKENS, TEMPERATURE, and more.
+            config (Config): Configuration parameters including MODEL_BIN_PATH, MODEL_TYPE, MAX_NEW_TOKENS, TEMPERATURE, and more.
 
         Returns:
             RetrievalQA: A configured RetrievalQA object.
@@ -154,12 +135,12 @@ class DocumentProcessor:
                                 input_variables=['context', 'question'])
         return prompt
 
-    def create_llm(self, config: Box):
+    def create_llm(self, config: Config):
         """
         Create a Language Model.
 
         Args:
-            config (Box): Configuration parameters including MODEL_BIN_PATH, MODEL_TYPE, MAX_NEW_TOKENS, TEMPERATURE, and more.
+            config (Config): Configuration parameters including MODEL_BIN_PATH, MODEL_TYPE, MAX_NEW_TOKENS, TEMPERATURE, and more.
 
         Returns:
             CTransformers: A configured CTransformers Language Model.
@@ -192,8 +173,8 @@ class DocumentProcessor:
         return dbqa
 
     def process_documents(self):
-        config = self.load_config()
-        dbqa = self.setup_dbqa(config)
+        
+        dbqa = self.setup_dbqa(self.config)
 
         start = timeit.default_timer() # Start timer
         # Parse input text into QA object
@@ -217,26 +198,36 @@ class DocumentProcessor:
         print(f"Time to retrieve response: {end - start}")
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
     parser.add_argument('input', type=str)
     parser.add_argument('--build-vectorstore', action='store_true',
                         help="Specify to build VectorStore. If not specified, it won't build VectorStore.")
+    parser.add_argument('--document-type', type=str, choices=["pdf", "md"], default="pdf",
+                        help="Specify the type of documents to process ('pdf' or 'md'). Default is 'pdf'.")
     args = parser.parse_args()
 
-    config_path = './config.yaml'
+    config_path = "./config.yaml"
+    config = Config.load_from_file(config_path)
     input_text = args.input
 
     # Initialize the DocumentProcessor
-    processor = DocumentProcessor(config_path, input_text)
+    processor = DocumentProcessor(config, input_text)
+
+
 
     if args.build_vectorstore:
+        # Initialize the VectorStoreBuilder
+        vectorstore_builder = VectorStoreBuilder(config, args.document_type)
+
+        # Build VectorStore
+        vectorstore_builder.build_vectorstore()
+    else:
         # Check if the vectorstore directory exists
         vectorstore_dir = './vectorstores/'
         if not os.path.exists(vectorstore_dir):
             logging.warning(f"'{vectorstore_dir}' directory not found. Building VectorStore is probably needed. Use '--build-vectorstore'.")
 
-        # Build VectorStore
-        processor.build_vectorstore(processor.load_config())
 
     # Process documents
     processor.process_documents()
